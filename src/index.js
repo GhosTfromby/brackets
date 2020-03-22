d554ec3c3a7b3b2e0a30e1fad129087967d9de35
Dzmitry Zaharouski <Iamdzmitry@gmail.com>
2020-03-22 20:30:45 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let getClosingBracket = (strPosition) => {
+    for(let i = 0; i < bracketsConfig.length; ++i)
+      if(str[strPosition] === bracketsConfig[i][0])
+        return bracketsConfig[i][1];
+    return -1;
+  }
+  let isClosingBracket = (strPosition) => {
+    for(let i = 0; i < bracketsConfig.length; ++i)
+      if(str[strPosition] === bracketsConfig[i][0])
+        return false;
+    return true;
+  }
+  for(let cursor = 0; cursor < str.length; ++cursor) {
+    if(stack.length && str[cursor] === getClosingBracket(stack[stack.length - 1])  ) {
+      stack.pop();
+    } else {
+      if(isClosingBracket(cursor)) {
+        return false;
+      } else {
+        stack.push(cursor);
+      }
+    }
+  }
+  if(stack.length)
+    return false;
+     
+  return true;
 }
